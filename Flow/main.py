264d164cc571a6fe879c7b6e34fa5054ae89e889
addygeek <ui22cs03@iiitsurat.ac.in>
2025-01-09 01:03:02 +0530

    added more flows and reverse enginnering it

---
+++ Flow/main.py
@@ -0,0 +1,10 @@
+from mira_sdk import MiraClient
+client = MiraClient(config={"API_KEY": "YOUR_API_KEY"})
+from dotenv import load_dotenv
+from mira_sdk import MiraClient
+import os
+
+load_dotenv()
+client = MiraClient(config={"API_KEY": os.getenv("MIRA_API_KEY")})
+
+print("RUN")
---
+++ Resume Analyser/main.py
@@ -0,0 +1,21 @@
+from mira_sdk import MiraClient, Flow
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# Initialize the client
+client = MiraClient(config={"API_KEY": os.getenv("MIRA_API_KEY")})
+
+version = "1.0.0"
+input_data = {
+    "resume_query": "What did john do at meta?"
+}
+
+# If no version is provided, latest version is used by default
+if version:
+    flow_name = f"@cosmic-labs/resume-analyser/{version}"
+else:
+    flow_name = "@cosmic-labs/resume-analyser"
+
+result = client.flow.execute(flow_name, input_data)
+print(result)
